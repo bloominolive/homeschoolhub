4d7c58ffec63d9b72adb8ce036ebcedf890aadad
bloominolive <bloomin.olive88@gmail.com>
2024-02-20 23:28:13 -0500

    updated add button color

--- app/components/AssignmentForm.jsx
+++ app/components/AssignmentForm.jsx
@@ -33,7 +33,7 @@ export default function AssignmentForm({students}){
                 />
             </div>
             
-            <button type="submit" className="bg-gray-600 hover:bg-gray-300 text-white hover:text-black font-bold py-2 px-4 rounded">
+            <button type="submit" className="bg-green-500 hover:bg-gray-300 text-white hover:text-black font-bold py-2 px-4 rounded">
                 Add Assignment
             </button>
         </form>     
--- app/components/JournalForm.jsx
+++ app/components/JournalForm.jsx
@@ -30,7 +30,7 @@ export default function JournalForm(){
                 />
             </div>
             
-            <button type="submit" className="bg-gray-600 hover:bg-gray-300 text-white hover:text-black font-bold py-2 px-4 rounded">
+            <button type="submit" className="bg-green-500 hover:bg-gray-300 text-white hover:text-black font-bold py-2 px-4 rounded">
                 Add Entry
             </button>
         </form>     
